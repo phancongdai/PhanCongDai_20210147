ea6924683ba70750b9c4e9813955410289047878
phancongdai <phandai645@gmail.com>
2024-05-18 20:43:42 +0700

    commit lab04

--- lab03/AimsProject/src/hust/soict/globalict/aims/media/Playable.java
+++ lab03/AimsProject/src/hust/soict/globalict/aims/media/Playable.java
@@ -1,7 +1,1 @@
 package hust.soict.globalict.aims.media;
-
-import hust.soict.globalict.aims.exception.PlayerException;
-
-public interface Playable {
-    public String play() throws PlayerException;
-}
